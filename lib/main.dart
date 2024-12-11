26dfbaf29aad30af97fda4aba3c4b418b7eb551f
tonygirgis <tonygirgis29@gmail.com>
2024-12-12 00:41:30 +0200

    add setup GetIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:bookly_mvvm/core/utils/app_colors.dart';
 import 'package:bookly_mvvm/core/utils/app_router.dart';
-import 'package:bookly_mvvm/features/home/presentation/views/home_screen.dart';
-import 'package:bookly_mvvm/features/splash/presentation/views/splash_screen.dart';
+import 'package:bookly_mvvm/core/utils/service_locator.dart';
+import 'package:bookly_mvvm/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_mvvm/features/home/presentation/manager/featured_books/featured_books_cubit.dart';
+import 'package:bookly_mvvm/features/home/presentation/manager/newest_books/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupGetIt();
   runApp(const MyApp());
 }
 
@@ -15,15 +18,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      title: 'Bookly',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(
-        useMaterial3: true,
-      ).copyWith(
-        scaffoldBackgroundColor: AppColors.scaffoldBackground,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),),
+        BlocProvider(create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>()),),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        title: 'Bookly',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(
+          useMaterial3: true,
+        ).copyWith(
+          scaffoldBackgroundColor: AppColors.scaffoldBackground,
+          textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
