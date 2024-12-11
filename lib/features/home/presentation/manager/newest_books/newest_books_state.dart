c9b1b8ad46afb1d642b7572604635e155a940dae
tonygirgis <tonygirgis29@gmail.com>
2024-12-12 00:15:34 +0200

    create newest_books_method

--- lib/features/home/presentation/manager/newest_books/newest_books_state.dart
+++ lib/features/home/presentation/manager/newest_books/newest_books_state.dart
@@ -3,18 +3,18 @@ part of 'newest_books_cubit.dart';
 @immutable
 sealed class NewestBooksState {
 
-  const NewestBooksState();
 }
 
+final class NewestBooksInitial extends NewestBooksState {}
 final class NewestBooksLoading extends NewestBooksState {}
 final class NewestBooksFailure extends NewestBooksState {
   final String errMsg;
 
-  const NewestBooksFailure(this.errMsg);
+   NewestBooksFailure(this.errMsg);
 }
 final class NewestBooksSuccess extends NewestBooksState {
 final List<BookModel> books;
 
-  const NewestBooksSuccess(this.books);
+   NewestBooksSuccess(this.books);
 
 }
